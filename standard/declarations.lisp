308a76611765ada1a8344f37bfe292adbcc5bbcf
James S <i@nixeagle.org>
2010-05-22 18:31:57 -0400

    Change DECLARE-SPEEDY to default with safety 1

--- standard/declarations.lisp
+++ standard/declarations.lisp
@@ -8,11 +8,12 @@
 
 
 (eval-always
-  (defun declare-speedy (&key (safety 0) (debug 1) (space 0) (speed 3))
+  (defun declare-speedy (&key (safety 1) (debug 1) (space 0) (speed 3))
     "Call with readtime eval to tell compiler to go fast and unsafe.
 
   * SPEED is set to 3, the max allowable.
-  * SAFETY is set to 0, so any types declared are trusted as is.
+  * SAFETY is set to 1, so any types declared are trusted as is,
+    basic argument checking is done.
   * DEBUG is set at 1, enough to give argument information for compiletime
     purposes (at least on sbcl).
   * SPACE is set to 0 to encourage inline expansion."
