f595cbc9593703f8e2dca3ff165d9119b85de8fc
James S <i@nixeagle.org>
2010-05-22 18:36:10 -0400

    Define DECLARE-SPEEDY-UNSAFE

--- standard/declarations.lisp
+++ standard/declarations.lisp
@@ -9,7 +9,7 @@
 
 (eval-always
   (defun declare-speedy (&key (safety 1) (debug 1) (space 0) (speed 3))
-    "Call with readtime eval to tell compiler to go fast and unsafe.
+    "Call with readtime eval to tell compiler to choose speed over safety.
 
   * SPEED is set to 3, the max allowable.
   * SAFETY is set to 1, so any types declared are trusted as is,
@@ -18,4 +18,14 @@
     purposes (at least on sbcl).
   * SPACE is set to 0 to encourage inline expansion."
     `(declare (optimize (speed ,speed) (space ,space) (compilation-speed 0)
-               (safety ,safety) (debug ,debug)))))
+               (safety ,safety) (debug ,debug))))
+  (defun declare-speedy-unsafe (&key (safety 0) (debug 1) (space 0) (speed 3))
+    "Call with readtime eval to tell compiler to go fast and unsafe.
+
+  * SPEED is set to 3, the max allowable.
+  * SAFETY is set to 1, so any types declared are trusted as is,
+    no checking is done at all!
+  * DEBUG is set at 1, enough to give argument information for compiletime
+    purposes (at least on sbcl).
+  * SPACE is set to 0 to encourage inline expansion."
+    (declare-speedy :safety safety :debug debug :space space :speed speed)))
